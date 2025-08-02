a55f477d55203ff4ba9e80e14aa62fa85b229a4b
3db2b57e64a3b393 <ushika.chaulagain@studytafensw.edu.au>
2025-08-02 16:37:19 +1000

    def greet(name):     """This function greets the person passed as an argument."""     return f"Hello, {name}!"
    # Calling the function
    message = greet("Alice")
    print(message)

--- ushi.py
+++ ushi.py
@@ -1,7 +1,9 @@
-def greet(name):
-    """This function greets the person passed as an argument."""
-    return f"Hello, {name}!"
+def calculate_area(a,b):
+    area = a*b
+    return area
 
-# Calling the function
-message = greet("Alice")
-print(message)
+def main():
+
+    print(calculate_area(2,4))
+if __name__ == "__main__":
+    main()
